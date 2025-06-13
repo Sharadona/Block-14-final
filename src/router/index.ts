7042e424f32d0bca28c37a1208302d876ae55aeb
Kankuro <pasha.kachanovskiy@mail.ru>
2025-06-13 23:12:43 +0500

    Инициализировал проект

---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory('https://7e91a37cae1a70b7.mokky.dev/'),
+  routes
+})
+
+export default router
