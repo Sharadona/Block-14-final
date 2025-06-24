24da9ba6965a809ca5265d3f8f76cb6fa5e5db9c
Шарадона <pasha.kachanovskiy@mail.ru>
2025-06-24 23:00:02 +0500

    Закончил авторизацию, добавил немного комментариев.

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,32 +3,46 @@ import {createStore} from "vuex";
 export default createStore({
   state(){
     return {
-      counter: 0,
+      account: {
+        email: '',
+        password: ''
+      },
       accounts: []
     }
   },
+  getters: {
+    // email: state => state.account.email,
+    // password: state => state.account.password,
+  },
   mutations: {
-    increment(state, n) {
-      state.counter += n
-    },
-    setAccount(state, account) {
-      state.accounts.push(account)
+    // increment(state, n) {
+    //   state.counter += n
+    // },
+
+    /*Передаю в массив ответ от сервера*/
+    setAccount(state, payload) {
+      state.accounts.push(payload)
+      // console.log(state.account)
+      // console.log(payload)
+      console.log(state.accounts)
     }
   },
   actions: {
-    checkAccount: async (context) => {
-      const checkAccount = {
-        email: 'ma@ma.ru',
-        password: '12345',
-      }
-
+    /*Отправляю запрос с аккаунтом на сервер для проверки
+    авторизирован ли данный пользователь*/
+    checkAccount: async (context, payload) => {
+      // const checkAccount = {
+      //   email: 'ma@ma.ru',
+      //   password: '12345',
+      // }
+      console.log(payload)
       const response = await fetch("https://7e91a37cae1a70b7.mokky.dev/auth", {
       method: "POST",
       headers: {
       Accept: "application/json",
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(checkAccount)
+      body: JSON.stringify(payload)
       });
       const account = await response.json();
       context.commit('setAccount', account);
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,15 +1,16 @@
+// Окно авторизации
 <template>
-  <form class="card container" @submit.prevent="check">
+  <form class="card container" @submit.prevent="checkAccount">
     <h1>Войти в систему</h1>
 
     <div class="form-control">
       <label for="email">Email</label>
-      <input type="email" id="email" v-model="email" required>
+      <input type="email" id="email" v-model="dataUser.email" required>
     </div>
 
     <div class="form-control">
       <label for="password">Пароль</label>
-      <input type="password" id="password" v-model="password" required>
+      <input type="password" id="password" v-model="dataUser.password" required>
     </div>
 
     <button class="btn primary" type="submit">Войти</button>
@@ -17,7 +18,6 @@
       Вы слишком часто пытаетесь войти в систему. Попробуйте позже
     </div>
   </form>
-  <button class="btn primary" @click="increment">Добавить {{counter}}</button>
 </template>
 
 <script>
@@ -28,21 +28,27 @@ export default {
   setup() {
     const store = useStore();
     const router = useRouter()
-    const email = ref('')
-    const password = ref('')
 
-    function check() {
-      if (email.value === 'ma@ma.ru' && password.value === '12345') {
+    const dataUser = ref({
+        email: '',
+        password: '',
+    })
+
+    /*Вызываю асинхронную функцию для проверки правильности введёного email и password
+    * Проверяю, соответствует email и password тем, что есть на сервере
+    * */
+    function checkAccount() {
+      store.dispatch("checkAccount", {
+        email: dataUser.value.email,
+        password: dataUser.value.password
+      })
+      if (dataUser.value.email === 'ma@ma.ru' && dataUser.value.password === '12345') {
         router.push('/navbar')
       }
     }
-
     return {
-      email,
-      password,
-      check,
-      counter: computed(() => store.state.counter),
-      increment: () => store.commit('increment', 1)
+      dataUser,
+      checkAccount
     }
   }
 }
